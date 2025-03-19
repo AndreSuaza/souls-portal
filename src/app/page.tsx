9604020162cd6bddf34895ca9ccc274e6aac5bd3
AsuazaDyN <asuaza@dichter-neira.com>
2025-03-18 21:22:09 -0500

    deploy versel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ export default function Home() {
   return (
     <div className="">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        
+        <h1>Bienvenido</h1>
       </main>
     </div>
   );
