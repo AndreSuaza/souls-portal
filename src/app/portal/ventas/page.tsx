eac0e75f6eacd29caa85b2c84c97c62ba431af40
AsuazaDyN <asuaza@dichter-neira.com>
2025-03-20 11:04:42 -0500

    gastos ok 1

--- src/app/portal/ventas/page.tsx
+++ src/app/portal/ventas/page.tsx
@@ -1,10 +1,8 @@
-import { VentasFrom } from "./ui/VentasFrom";
 
 export default function Ventas() {
   return (
     <>
     <h1 className="text-2xl my-6 font-bold">Ventas</h1>
-    <VentasFrom categories={[]}/> 
     </>
   );
 }
